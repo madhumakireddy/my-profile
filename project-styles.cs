8307817107483403502e5e63f1754980a66545e9
madhumakireddy <101985626+madhumakireddy@users.noreply.github.com>
2024-10-16 16:52:10 -0400

    project-styles.css

---
+++ project-styles.cs
@@ -0,0 +1,21 @@
+/* project-styles.css */
+body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Poppins', sans-serif;
+    background-color: #1b1b1b;
+    color: white;
+}
+
+.project-header {
+    text-align: center;
+    padding: 50px;
+}
+
+.project-description {
+    padding: 20px;
+}
+
+.project-3d {
+    width: 100%;
+    height: 500px;
